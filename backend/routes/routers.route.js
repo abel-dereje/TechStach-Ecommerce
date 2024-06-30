bab6e06a44431c998aa5de177d8758ec1cda14b6
fullstackdevelopmentforall <abel.dereje94@gmail.com>
2024-07-01 00:04:16 +0300

    Creating router for user logout

--- backend/routes/routers.route.js
+++ backend/routes/routers.route.js
@@ -4,17 +4,20 @@ const router = express.Router()
 
 const userSignUp = require("../controllers/user/user-signup.controller");
 const loginUsers = require("../controllers/user/user-signin.controller");
+const userDetails = require("../controllers/user/user-details.controller");
+const userLogout = require('../controllers/user/user-logout.controller');
 
 
 
 router.post('/register', userSignUp);
 router.post('/login', loginUsers);
-// UserRouter.get('/api/users', getUsers);
+router.get('/users', userDetails);
+router.post('/logout', userLogout);
+
 // UserRouter.get('/api/user/:id', getUser);
 // UserRouter.put('/api/status/:id', userStatus);
 // UserRouter.put('/api/updateUser/:id', updateUser);
 // UserRouter.delete('/api/deleteUser/:id', deleteUser);
-// UserRouter.post('/api/logout', logoutUsers);
 
 module.exports = router;
 
