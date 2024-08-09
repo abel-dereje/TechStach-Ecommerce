4b6560ce30495c16c708fce1c3d006d3ecd88844
fullstackdevelopmentforall <abel.dereje94@gmail.com>
2024-08-09 23:54:07 +0300

    Adding a link on the trending product page

--- frontend/src/Pages/ContentAreaOne.js
+++ frontend/src/Pages/ContentAreaOne.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 import SummaryApi from '../common';
 import axios from 'axios';
 import { Link } from 'react-router-dom';
-import displayINRCurrency from '../helpers/displayCurrency';
+import displayCurrency from '../helpers/displayCurrency';
 
 const TrendingProducts = () => {
   const [trendingProducts, setTrendingProducts] = useState([]);
@@ -51,7 +51,7 @@ const TrendingProducts = () => {
       <h2 className="text-xl font-bold mb-4">Trending Products</h2>
       <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
         {limitedProducts.map((product, index) => (
-          <Link to="#add" key={index} className="text-white px-4 py-2 rounded flex items-center">
+          <Link to={`/product/${product._id}`} key={index} className="text-white px-4 py-2 rounded flex items-center">
             <div className="flex items-center space-x-4 mb-4">
               {product.productImage && product.productImage.length > 0 ? (
                 <img
@@ -64,8 +64,8 @@ const TrendingProducts = () => {
               )}
               <div>
                 <p className="font-bold text-slate-600 hover:bg-white hover:text-[#009FBD]">{product.productName}</p>
-                <p className="text-sm line-through text-slate-600">{displayINRCurrency (159.99)}</p>
-                <p className="font-bold text-slate-600 hover:bg-white hover:text-[#009FBD]">{displayINRCurrency(product.price)}</p>
+                <p className="text-sm line-through text-slate-600">{displayCurrency (159.99)}</p>
+                <p className="font-bold text-slate-600 hover:bg-white hover:text-[#009FBD]">{displayCurrency(product.price)}</p>
               </div>
             </div>
           </Link>
